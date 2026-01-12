146f9eb5e0cd21e318ae78f550afafaa718c56db
Toster <toster.desing@gmail.com>
2026-01-11 21:17:06 -0500

    DISEÑO FINAL

--- js/sketch.js
+++ js/sketch.js
@@ -4,7 +4,7 @@ let RETICULAY;
 let ancho; //altura de celda
 let alto; //anchura de celda
 
-const NA = 23; // número de azulejos
+const NA = 25; // número de azulejos
 const fileName = 'tile';
 const fileExtension = '.png';
 let reglas = [];
